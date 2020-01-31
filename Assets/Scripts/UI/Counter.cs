d0e0d2d5be5c97b7de47c2c95d69a0b44630b43e
Fx <fx73000@yahoo.fr>
2020-01-31 22:19:51 +0100

    Ui time  + count

---
+++ Assets/Scripts/UI/Counter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Counter : MonoBehaviour
+{
+
+    public int _count = 0;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        this.gameObject.GetComponent<TextMeshProUGUI>().text = _count.ToString();
+    }
+    public void Add()
+    {
+        _count++;
+        this.gameObject.GetComponent<TextMeshProUGUI>().text = _count.ToString();
+    }
+}
--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float _time = 60;
+    public int _timetoadd = 30;
+    private bool end = false;
+
+    private void Start()
     {
-        
+        this.gameObject.GetComponent<TextMeshProUGUI>().text = System.Math.Truncate(_time).ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _time -= Time.deltaTime;
+        this.gameObject.GetComponent<TextMeshProUGUI>().text = System.Math.Truncate(_time).ToString();
+        if (!end && _time <= 0)
+        {
+            end = true; //Fun to end
+        }
     }
+
+    public void AddTime()
+    {
+        _time += _timetoadd;
+    }
+
+    public void TimerReset(float t = 60)
+    {
+        _time = 60;
+        end = false;
+    }
+
 }
