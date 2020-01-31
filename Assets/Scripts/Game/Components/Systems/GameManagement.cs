795d3a7d43113a8336eb4dfe6ebc8c8e390e54ab
Fx <fx73000@yahoo.fr>
2020-01-31 23:21:30 +0100

    Added GameManagement
    launch and end game

---
+++ Assets/Scripts/Game/Components/Systems/GameManagement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManagement : MonoBehaviour
+{
+    private float _time = 0;
+    private int _startingtime = 3;
+    private TextMeshProUGUI _tmp;
+    private bool _active = true;
+
+    //Objets geres
+    Timer _Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tmp = Instantiate(Resources.Load<GameObject>("Text (TMP)"), GameObject.FindGameObjectWithTag("Canvas").transform).GetComponent<TextMeshProUGUI>();
+        _tmp.text = "Get Ready !";
+        _tmp.fontSize = 80;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_active)
+        {
+            _time += Time.deltaTime;
+            if (_time >= 1)
+            {
+                _tmp.text = (_startingtime--).ToString();
+                _time = 0;
+                if (_startingtime == -1)
+                    _tmp.text = "GO";
+                if (_startingtime == -2)
+                {
+                    Destroy(_tmp.gameObject);
+                    _active = false;
+                    _Timer.TimerReset();
+                }
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        _tmp = Instantiate(Resources.Load<GameObject>("Text (TMP)"), GameObject.FindGameObjectWithTag("Canvas").transform).GetComponent<TextMeshProUGUI>();
+        _tmp.text = "Fini !!";
+
+        StartCoroutine(WaitForClose());
+        Application.Quit();
+
+    }
+    IEnumerator WaitForClose()
+    {
+        yield return new WaitForSeconds(5);
+    }
+
+
+
+    public void RegisterTimer(Timer t)
+    {
+        _Timer = t;
+    }
+}
--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -7,21 +7,26 @@ public class Timer : MonoBehaviour
 {
     public float _time = 60;
     public int _timetoadd = 30;
-    private bool end = false;
+    private bool end = true;
 
     private void Start()
     {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GameManagement>().RegisterTimer(this);
         this.gameObject.GetComponent<TextMeshProUGUI>().text = System.Math.Truncate(_time).ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _time -= Time.deltaTime;
-        this.gameObject.GetComponent<TextMeshProUGUI>().text = System.Math.Truncate(_time).ToString();
-        if (!end && _time <= 0)
+        if (!end)
         {
-            end = true; //Fun to end
+            _time -= Time.deltaTime;
+            this.gameObject.GetComponent<TextMeshProUGUI>().text = System.Math.Truncate(_time).ToString();
+            if (_time <= 0)
+            {
+                end = true;
+                GameObject.FindGameObjectWithTag("Canvas").GetComponent<GameManagement>().EndGame();
+            }
         }
     }
 
