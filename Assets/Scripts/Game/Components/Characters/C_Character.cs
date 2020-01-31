a8a281b89ad21edb87d727e0dbeda6cbb3ad839f
Lysfith <lysfith@gmail.com>
2020-02-01 00:17:31 +0100

    Add hand

--- Assets/Scripts/Game/Components/Characters/C_Character.cs
+++ Assets/Scripts/Game/Components/Characters/C_Character.cs
@@ -16,6 +16,7 @@ namespace Assets.Scripts.Game.Components.Characters
 
         [SerializeField] private C_CharacterMover _mover;
         [SerializeField] private C_CharacterControl _control;
+        [SerializeField] private C_CharacterHand _hand;
 
         public C_CharacterStats Stats => _stats;
         public C_CharacterMover Mover => _mover;
@@ -26,18 +27,22 @@ namespace Assets.Scripts.Game.Components.Characters
             _stats = GetComponent<C_CharacterStats>();
             _mover = GetComponent<C_CharacterMover>();
             _control = GetComponent<C_CharacterControl>();
+            _hand = GetComponent<C_CharacterHand>();
 
             Assert.IsNotNull(_mover);
             Assert.IsNotNull(_control);
+            Assert.IsNotNull(_hand);
 
             _mover.Enable();
             _control.Enable();
+            _hand.Enable();
         }
 
         private void OnDisable()
         {
             _mover.Disable();
             _control.Disable();
+            _hand.Disable();
         }
     }
 }
---
+++ Assets/Scripts/Game/Components/Characters/Parts/C_CharacterHand.cs
@@ -0,0 +1,96 @@
+﻿using Assets.Scripts.Game.Components.Characters.Triggers;
+using Assets.Scripts.Game.Components.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+namespace Assets.Scripts.Game.Components.Characters.Parts
+{
+    public class C_CharacterHand : C_CharacterPart
+    {
+        [SerializeField] private C_Character _character;
+
+        [SerializeField] private Transform _hand;
+        [SerializeField] private C_TriggerHand _triggerHand;
+        [SerializeField] private C_Object _object;
+
+        private void OnEnable()
+        {
+            _character = GetComponent<C_Character>();
+
+            Assert.IsNotNull(_character);
+
+            EnableHand();
+        }
+
+        private void EnableHand()
+        {
+            _triggerHand.gameObject.SetActive(true);
+        }
+
+        private void DisableHand()
+        {
+            _triggerHand.gameObject.SetActive(false);
+        }
+
+        public void Take()
+        {
+            if(!_isEnabled)
+            {
+                return;
+            }
+
+            if(_triggerHand.CurrentObject == null)
+            {
+                return;
+            }
+
+            _object = _triggerHand.CurrentObject;
+            _object.Take();
+            _object.transform.SetParent(_hand);
+
+            DisableHand();
+        }
+
+        public void Release()
+        {
+            if (!_isEnabled)
+            {
+                return;
+            }
+
+            if (_object == null)
+            {
+                return;
+            }
+
+            _object.transform.SetParent(null);
+            _object.Release();
+
+            EnableHand();
+        }
+
+        public void Throw()
+        {
+            if (!_isEnabled)
+            {
+                return;
+            }
+
+            if (_object == null)
+            {
+                return;
+            }
+
+            _object.transform.SetParent(null);
+            _object.Throw(transform.forward);
+
+            EnableHand();
+        }
+    }
+}
