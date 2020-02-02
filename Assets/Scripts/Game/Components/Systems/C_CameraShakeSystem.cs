0abc1d92b790610b2354c131f18c0d3de74f93ad
Lysfith <lysfith@gmail.com>
2020-02-02 12:08:56 +0100

    Ajout du camera shake

---
+++ Assets/Scripts/Game/Components/Systems/C_CameraShakeSystem.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Assets.Scripts.Game.Components.Systems
+{
+    public class C_CameraShakeSystem : MonoBehaviour
+    {
+        private static C_CameraShakeSystem _instance;
+
+        public static C_CameraShakeSystem Instance => _instance;
+
+
+        [SerializeField] Transform _camera;
+        [SerializeField] Vector3 _shakeForce;
+
+        private void OnEnable()
+        {
+            _instance = this;
+
+            Assert.IsNotNull(_camera);
+        }
+
+        public void Shake()
+        {
+            _camera.DOShakePosition(10f, _shakeForce, 5, 10f, false, true);
+        }
+    }
+}
