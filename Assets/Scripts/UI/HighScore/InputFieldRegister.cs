b129cc0bcbe93a9d33976e6911df0f7115398077
Fx <fx73000@yahoo.fr>
2020-02-02 02:44:13 +0100

    Added HighScore

---
+++ Assets/Scripts/UI/HighScore/InputFieldRegister.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InputFieldRegister : MonoBehaviour
+{
+    public void Validate()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GameManagement>().SendHighScore(GetComponent<TMP_InputField>());
+    }
+}
