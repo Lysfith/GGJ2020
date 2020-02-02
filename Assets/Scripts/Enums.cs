fbe4115e3860510ac41ff1e5c933a84453f2acda
Lysfith <lysfith@gmail.com>
2020-02-02 16:21:51 +0100

    AJout de la génération des robots

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -12,14 +12,15 @@ namespace Assets.Scripts
         CHEST,
         LEFT_ARM,
         RIGHT_ARM,
+        LEG,
         WASTE,
         BONUS
     }
 
-    public enum BotType
+    public enum PartVersion
     {
-        None,
-        Type1
+        V1,
+        V2
     }
 
     public enum PlayerType
