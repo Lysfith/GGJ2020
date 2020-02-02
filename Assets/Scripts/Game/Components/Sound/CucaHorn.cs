07b201a35ad54cbd675df54a887b99b90bacd89b
Fx <fx73000@yahoo.fr>
2020-02-02 17:34:46 +0100

    horn

---
+++ Assets/Scripts/Game/Components/Sound/CucaHorn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CucaHorn : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SoundManager.PlaySound(SoundList.Sound.cucaracha);
+    }
+
+}
